bebceca511e957f4911581383c8d7b380ade1dbe
JohnP <hprobotic@gmail.com>
2016-10-17 10:13:21 +0700

    completed cat ffunc

---
+++ cat.js
@@ -0,0 +1,10 @@
+require('./helper')
+let fs = require('fs').promise
+
+async function cat() {
+    let input = await process.argv[2]
+    let data = await fs.readFile(input, 'utf-8')
+    console.log(data)
+}
+
+cat()
--- echo.js
+++ echo.js
@@ -7,7 +7,8 @@ async function echo() {
     // Use 'await' in here
     // Your implementation here
     let input = await process.argv.slice(2).join(" ")
-    console.log(await fs.readFile(__filename, console.log))
+    console.log(input)
+    // console.log(await fs.readFile(__filename, console.log))
 }
 
 echo()
