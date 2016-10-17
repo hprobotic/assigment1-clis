8f36dc971218ac8b91be681d68864c6f23ff58bf
JohnP <hprobotic@gmail.com>
2016-10-17 18:22:56 +0700

    Add walkthrough gif file

--- mkdir.js
+++ mkdir.js
@@ -5,7 +5,7 @@ let fs = require('fs').promise
 
 async function mkdir() {
     let argv = await process.argv
-    console.log(argv)
+    // console.log(argv)
     let input = await process.argv[2]
     await fs.mkdir(input)
 }
