319bf432183a3b39423a2d9b78f0a3cae2c85b89
JohnP <hprobotic@gmail.com>
2016-10-17 10:09:51 +0700

    completed echo func

--- echo.js
+++ echo.js
@@ -6,6 +6,7 @@ let fs = require('fs').promise
 async function echo() {
     // Use 'await' in here
     // Your implementation here
+    let input = await process.argv.slice(2).join(" ")
     console.log(await fs.readFile(__filename, console.log))
 }
 
